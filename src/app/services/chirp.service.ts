9efc18495f74d3f027b8803b280db8b4d0fde444
BRPickens <brpickens@gmail.com>
2017-11-20 14:37:09 -0600

    fix this lovelight

--- src/app/services/chirp.service.ts
+++ src/app/services/chirp.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import { IChirp } from '../chirpInterface';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ChirpService {
 
-    static api = 'http://localhost:3000/api/chirps';
+    private api = 'http://localhost:3000/api/chirps';
 
     constructor (private http: HttpClient) {
 
@@ -15,19 +16,19 @@ export class ChirpService {
     chirps: Array<IChirp> = [];
 
     getChirps() {
-
+        return this.http.get(this.api);
     }
 
     getChirp(id: string) {
-
+        return this.http.get(`${this.api}/${id}`);
     }
 
     deleteChirp(id: string) {
-
+        return this.http.delete(`${this.api}/${id}`);
     }
 
-    createChirp() {
-
+    createChirp(chirp: IChirp): Observable<any> {
+        return this.http.post(this.api, chirp);
     }
 }
 
