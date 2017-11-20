8f3659cd839ea1c6d20fa6c2b32e6c63d20ecf36
LovelightCross <32072875+LovelightCross@users.noreply.github.com>
2017-11-20 15:21:46 -0600

    connected server to client. list component functional

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,24 +2,30 @@ import { Component, OnInit, Output } from '@angular/core';
 import { NgForOf } from '@angular/common';
 import { ChirpService } from '../services/chirp.service';
 import { IChirp } from '../chirpInterface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
+
+
 export class ListComponent implements OnInit {
-  chirps: IChirp[];
+  chirps: any;
 
   constructor(private chirpService: ChirpService,
   ) { }
 
 
   getChirps(): void {
-  }
+    this.chirpService.getChirps()
+      .subscribe((response) => this.chirps = response);
+    
+    }
 
   ngOnInit(): void {
     this.getChirps();
+    }
   }
 
-}
