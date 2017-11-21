9fa7b54c77ec94cd24aa6c919818a0ad072d2c02
DWW1993 <daniel.w.walker1993@gmail.com>
2017-11-21 15:31:24 -0600

    fixed login page error

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -23,7 +23,7 @@ export class LoginComponent implements OnInit {
     });
    }
 
-//   ngOnInit() {
+  ngOnInit() {
 // this.userService.spGetUsers()
 // .subscribe((response)=>this.users=response);
 
