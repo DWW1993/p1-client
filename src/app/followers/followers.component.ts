90daade7c344dfdd24be3b45209d7aa36875b482
DWW1993 <daniel.w.walker1993@gmail.com>
2017-11-28 10:18:57 -0600

    working on followers

--- src/app/followers/followers.component.ts
+++ src/app/followers/followers.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from "../services/user.service";
+import { IUser } from "../userInterface";
+import { FollowersService } from "../services/followers.service";
+
 @Component({
   selector: 'app-followers',
   templateUrl: './followers.component.html',
   styleUrls: ['./followers.component.scss']
 })
 export class FollowersComponent implements OnInit {
+  user: any;
+  followingChirps: any;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private followersService: FollowersService
+  ) {}
 
   ngOnInit() {
+    this.userService.me(true)
+    .then((user: IUser) => {
+      this.user = user;
+    });   
+    this.followersService.getFollowingChirps(this.user.id)
+    .subscribe((response) => {
+      this.followingChirps = response
+      console.log(this.followingChirps)
+    });  
   }
 
 }
--- src/app/services/followers.service.ts
+++ src/app/services/followers.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { isNil } from 'lodash';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class FollowersService {
+  private api = '/api/followers';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  getFollowingChirps(followerId: number): Observable<any> {
+    return this.http.get(`${this.api}/${+followerId}`);
+  }
 
 }
